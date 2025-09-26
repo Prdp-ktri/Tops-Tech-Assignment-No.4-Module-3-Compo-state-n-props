c98cf2ac269ff4a859c53a0158d08d11330be0c5
Pradeep Khatri <pradeepkhatri44@live.com>
2025-09-26 17:46:18 +0530

    commit 05-lifecycle-method
    commit 05-lifecycle-method

--- 05-lifecycle-methods/Lab-Assignments/02-update-n-unmount/src/App.jsx
+++ 05-lifecycle-methods/Lab-Assignments/02-update-n-unmount/src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Component } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-    </>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
 }
-
-export default App
